d82c478c64fa1cca55db4e87b7f81de25803e0c2
yossrmohammed <yossr.khatab5@gmail.com>
2024-01-27 00:38:54 +0200

    enhance design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,8 @@ export const routes: Routes = [
     {
         path:"prduct-details/:id",
         component:ProductDetailsComponent
+    },{
+        path:'about-us',
+        component:AboutUsComponent
     }
 ];
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,17 +2,39 @@ import { Component } from '@angular/core';
 import productData from '../../../products-list.json'
 import { ProductInterface } from '../interfaces/product-interface';
 import { ActivatedRoute } from '@angular/router';
+import { GalleriaModule } from 'primeng/galleria';
+import { FormsModule } from '@angular/forms';
+import { RatingModule } from 'primeng/rating';
+import { TagModule } from 'primeng/tag';
+import { ButtonModule } from 'primeng/button';
+import { InputNumberModule } from 'primeng/inputnumber';
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [],
+  imports: [GalleriaModule, FormsModule,RatingModule,TagModule,ButtonModule,InputNumberModule],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
 export class ProductDetailsComponent {
+  quantity: number = 1;
   products:ProductInterface[]= productData;
   productDetails ?: ProductInterface
-  constructor(private activeRoute : ActivatedRoute){}
+  responsiveOptions: any[] = [
+    {
+        breakpoint: '1024px',
+        numVisible: 5
+    },
+    {
+        breakpoint: '768px',
+        numVisible: 3
+    },
+    {
+        breakpoint: '560px',
+        numVisible: 1
+    }
+];;
+  images: any[] | undefined;
+  constructor(private activeRoute : ActivatedRoute ,){}
   ngOnInit(){
     
     
@@ -22,5 +44,8 @@ export class ProductDetailsComponent {
     //console.log('ID from Route:', id);
     //console.log('All Products:', this.products);
     //console.log('Product Details:', this.productDetails);
+    this.images=this.productDetails?.images
+
   }
+  
 }
